22d5a286de14d3c45c35c09ac87181ed31e0288b
Garrett <AdultVHS@gmail.com>
2023-08-13 23:42:19 -0500

    WORKING API VARIABLES

--- index.js
+++ index.js
@@ -4,13 +4,39 @@
     //         console.log(data.data[0])
     //     })
 
-async function getData() { await fetch ('http://api.weatherapi.com/v1/current.json?key=4a2e77ab11604a74b5470137231308&q=Austin, TX&aqi=no')
-                            .then(r => r.json())
-                            .then(data => {
-                                console.log(data);
-                                console.log(data.location);
-                                console.log(data.current);
-                            })
-                            
-                        }
+//async call, distinct vaiables for the parsing
+// async function getData() { 
+//     const res = await fetch ('http://api.weatherapi.com/v1/current.json?key=4a2e77ab11604a74b5470137231308&q=Austin, TX&aqi=no');
+//     let data = await res.json();
+//     return data;
+//                         };
+
+
+//'long hand' verison test
+let loc;
+let cur;
+
+fetch('http://api.weatherapi.com/v1/current.json?key=4a2e77ab11604a74b5470137231308&q=Austin, TX&aqi=no')
+    .then(function (responce){
+        return responce.json();
+    })
+    .then(function (dataObj){
+        console.log(typeof(loc));
+        console.log(typeof(cur));
+        console.log(dataObj.location);
+        console.log(dataObj.current);
+        loc = dataObj.location;
+        cur = dataObj.current;
+        console.log(typeof(loc));
+        console.log(typeof(cur));
+    })//catch error obj and alert
+    .catch(function (error){
+        console.log('Oh snap there is an issue');
+        console.log( error );
+    });
+    
+// create data variable with await b/c the fetch is asnyc in console not script(?)
+// let data = await getData();
+//TO DO
+// learn async handing 
 
