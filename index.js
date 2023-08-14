338cb58578892f35175a251ffaf7ac9f2aae8cdf
Garrett <AdultVHS@gmail.com>
2023-08-14 00:05:59 -0500

    working in the most bare way possible

--- index.js
+++ index.js
@@ -12,6 +12,12 @@
 //                         };
 
 
+//html var
+const htmlLocation = document.getElementById('location');
+const htmlCurrent = document.getElementById('current');
+const htmlHigh = document.getElementById('high');
+const htmlLow = document.getElementById('low');
+
 //'long hand' verison test
 let loc;
 let cur;
@@ -29,12 +35,24 @@ fetch('http://api.weatherapi.com/v1/current.json?key=4a2e77ab11604a74b5470137231
         cur = dataObj.current;
         console.log(typeof(loc));
         console.log(typeof(cur));
+        htmlLocation.innerText = (`${loc.name} state of ${loc.region}`);
+        htmlCurrent.innerText = (`Current Temp is ${cur.temp_f}`);
+        htmlHigh.innerText = ('Working!');
+        htmlLow.innerText = ('Working!');
     })//catch error obj and alert
     .catch(function (error){
         console.log('Oh snap there is an issue');
         console.log( error );
     });
-    
+   
+
+
+// trying to change UI after fetch
+// htmlLocation.innerText = (`${loc.name}state of ${loc.region}`);
+// htmlCurrent.innerText = (`Current Temp is ${cur.temp_f}`);
+// htmlHigh.innerText = ('Working!');
+// htmlLow.innerText = ('Working!');
+
 // create data variable with await b/c the fetch is asnyc in console not script(?)
 // let data = await getData();
 //TO DO
