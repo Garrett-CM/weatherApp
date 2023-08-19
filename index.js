447b7ee27c933a6a293a186d9e53892b6b42d888
Garrett <AdultVHS@gmail.com>
2023-08-18 23:08:18 -0500

    daily

--- index.js
+++ index.js
@@ -21,24 +21,17 @@ const htmlLow = document.getElementById('low');
 //'long hand' verison test
 let loc;
 let cur;
-
+//fetch data and update varaibles
 fetch('http://api.weatherapi.com/v1/current.json?key=4a2e77ab11604a74b5470137231308&q=Austin, TX&aqi=no')
     .then(function (responce){
         return responce.json();
     })
     .then(function (dataObj){
-        console.log(typeof(loc));
-        console.log(typeof(cur));
         console.log(dataObj.location);
         console.log(dataObj.current);
         loc = dataObj.location;
-        cur = dataObj.current;
-        console.log(typeof(loc));
-        console.log(typeof(cur));
         htmlLocation.innerText = (`${loc.name} state of ${loc.region}`);
         htmlCurrent.innerText = (`Current Temp is ${cur.temp_f}`);
-        htmlHigh.innerText = ('Working!');
-        htmlLow.innerText = ('Working!');
     })//catch error obj and alert
     .catch(function (error){
         console.log('Oh snap there is an issue');
