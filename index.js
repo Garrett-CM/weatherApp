30bfe6d095c960da334439acb0f6cb5cc6e324ad
Garrett <AdultVHS@gmail.com>
2023-08-22 23:31:58 -0500

    added api data, rough v1

--- index.js
+++ index.js
@@ -13,25 +13,35 @@
 
 
 //html var
-const htmlLocation = document.getElementById('location');
-const htmlCurrent = document.getElementById('current');
-const htmlHigh = document.getElementById('high');
-const htmlLow = document.getElementById('low');
+const htmlLocation = document.getElementById('current-condition');
+const htmlCurrent = document.getElementById('current-temp');
+const htmlHigh = document.getElementById('temp-high');
+const htmlLow = document.getElementById('temp-low');
+const htmlAvg = document.getElementById('temp-avg');
+const htmlWind = document.getElementById('wind');
 
 //'long hand' verison test
 let loc;
 let cur;
 //fetch data and update varaibles
-fetch('http://api.weatherapi.com/v1/current.json?key=4a2e77ab11604a74b5470137231308&q=Austin, TX&aqi=no')
+fetch('http://api.weatherapi.com/v1/forecast.json?key=4a2e77ab11604a74b5470137231308&q=Austin&days=1&aqi=no&alerts=no')
     .then(function (responce){
         return responce.json();
     })
     .then(function (dataObj){
         console.log(dataObj.location);
         console.log(dataObj.current);
-        loc = dataObj.location;
-        htmlLocation.innerText = (`${loc.name} state of ${loc.region}`);
-        htmlCurrent.innerText = (`Current Temp is ${cur.temp_f}`);
+
+        locationData = dataObj.location;
+        currentData = dataObj.current;
+        forecastData = dataObj.forecast;
+
+        htmlLocation.innerText = (`${locationData.name} state of ${locationData.region}`);
+        htmlCurrent.innerText = (`Current Temp is ${currentData.temp_f}`);
+        htmlHigh.innerText = (`${forecastData.forecastday[0].day.maxtemp_f}`);
+        htmlLow.innerText = (`${forecastData.forecastday[0].day.mintemp_f}`);
+        htmlAvg.innerText = (`${forecastData.forecastday[0].day.avgtemp_f}`);
+        htmlWind.innerText = (`${forecastData.forecastday[0].day.maxwind_mph}`);
     })//catch error obj and alert
     .catch(function (error){
         console.log('Oh snap there is an issue');
